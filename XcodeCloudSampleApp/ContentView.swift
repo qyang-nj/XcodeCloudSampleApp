7986a859cada88d4506fd52ec439d878eff56032
Qing Yang <yangq.nj@gmail.com>
2022-03-11 10:44:40 -0800

    first commit

---
+++ XcodeCloudSampleApp/ContentView.swift
@@ -0,0 +1,17 @@
+// Created by Qing Yang on 3/11/22.
+// Copyright © 2022 Airbnb Inc. All rights reserved.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeCloudSampleApp/XcodeCloudSampleAppApp.swift
@@ -0,0 +1,13 @@
+// Created by Qing Yang on 3/11/22.
+// Copyright © 2022 Airbnb Inc. All rights reserved.
+
+import SwiftUI
+
+@main
+struct XcodeCloudSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
