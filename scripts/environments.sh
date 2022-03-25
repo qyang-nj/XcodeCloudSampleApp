1501dd36bb4afae7701407877116cd3d046d6b45
Qing Yang <yangq.nj@gmail.com>
2022-03-25 13:58:08 -0700

    update

--- scripts/environments.sh
+++ scripts/environments.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "## All environment variables:" && env
 echo "----------"
+
 echo "## Xcode version:" && xcodebuild -version
 echo "----------"
+
 echo "## Swift version:" && swiftc -version
 echo "----------"
+
 echo "## Ruby version:" && ruby --version
 echo "----------"
+
 echo "## Python version:" && python --version; python3 --version
 echo "----------"
+
 echo "## Java version:" &&  which java && java -version
 echo "----------"
